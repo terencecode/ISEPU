c5f6a3d36f276d2f3cd91bf472970c426beb2009
William Trojanowski <williamtrojanowski@MacBook-Pro-de-William.local>
2019-05-06 12:59:15 +0200

    Initialization

---
+++ src/main/java/com/equipeor/isepu/IsepuApplication.java
@@ -0,0 +1,13 @@
+package com.equipeor.isepu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsepuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsepuApplication.class, args);
+	}
+
+}
