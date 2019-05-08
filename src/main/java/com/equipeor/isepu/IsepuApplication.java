544cb4bb25bdd923b39a64f8ae3b6ec05046b1b2
cleman95 <clement.phu@hotmail.fr>
2019-05-08 11:39:11 +0200

    test creation eleve

--- src/main/java/com/equipeor/isepu/IsepuApplication.java
+++ src/main/java/com/equipeor/isepu/IsepuApplication.java
@@ -3,11 +3,31 @@ package com.equipeor.isepu;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.persistence.Entity;
+
+
 @SpringBootApplication
 public class IsepuApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(IsepuApplication.class, args);
+	}
+
+}
+
+@Entity
+ class Eleve{
+	private  Long id;
+	private String firstName;
+	private String lastName;
+	private String description;
+
+	Eleve() {}
+
+	Eleve(String firstName, String lastName, String description) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.description = description;
 	}
 
 }
---
+++ src/test/java/com/equipeor/isepu/EleveTest.java
@@ -0,0 +1,13 @@
+package com.equipeor.isepu;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class EleveTest extends IsepuApplicationTests {
+    @Test
+    public void test(){
+        new Eleve("clement","Phu","A2");
+    }
+
+}
