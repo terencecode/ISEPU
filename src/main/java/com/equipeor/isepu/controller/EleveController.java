4fa660826b653ab2c818d51705f7909f12635a5d
William Trojanowski <williamtrojanowski.wt@gmail.com>
2019-05-13 13:01:48 +0200

    Add http response code for save eleve endpoint

--- src/main/java/com/equipeor/isepu/controller/EleveController.java
+++ src/main/java/com/equipeor/isepu/controller/EleveController.java
@@ -3,10 +3,14 @@ package com.equipeor.isepu.controller;
 import com.equipeor.isepu.dao.EleveDao;
 import com.equipeor.isepu.model.Eleve;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -15,8 +19,28 @@ public class EleveController {
     @Autowired
     private EleveDao eleveDao;
 
-    @RequestMapping(value="/Eleves", method= RequestMethod.GET)
+    @GetMapping(value="/Eleves")
     public List<Eleve> listeEleves() {
         return eleveDao.findAll();
     }
+
+    //ajouter un produit
+    @PostMapping(value = "/Eleves")
+    public ResponseEntity<Void> ajouterProduit(@RequestBody Eleve product) {
+
+        Eleve eleveAdded =  eleveDao.save(product);
+
+        if (eleveAdded == null)
+            return ResponseEntity.noContent().build();
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(eleveAdded.getId())
+                .toUri();
+
+        return ResponseEntity.ok().build();
+    }
+
+
 }
