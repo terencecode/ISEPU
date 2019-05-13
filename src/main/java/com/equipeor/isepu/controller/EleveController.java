e0477777cf0497e71c7b9f95d4b25a8da6b6f6b5
William Trojanowski <williamtrojanowski.wt@gmail.com>
2019-05-13 11:51:32 +0200

    First endpoint

--- src/main/java/com/equipeor/isepu/controller/EleveController.java
+++ src/main/java/com/equipeor/isepu/controller/EleveController.java
@@ -1,13 +1,22 @@
 package com.equipeor.isepu.controller;
 
+import com.equipeor.isepu.dao.EleveDao;
+import com.equipeor.isepu.model.Eleve;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class EleveController {
-    @RequestMapping(value="/Produits", method= RequestMethod.GET)
-    public String listeEleves() {
-        return "Un exemple d'élève";
+
+    @Autowired
+    private EleveDao eleveDao;
+
+    @RequestMapping(value="/Eleves", method= RequestMethod.GET)
+    public List<Eleve> listeEleves() {
+        return eleveDao.findAll();
     }
 }
