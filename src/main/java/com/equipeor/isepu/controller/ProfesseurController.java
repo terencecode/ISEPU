29d8696197b9b9a2ff2540c998a2bafe09450f5a
cleman95 <clement.phu@hotmail.fr>
2019-05-15 23:00:51 +0200

    Bug autowired
    Bug in the auto wired with declaration of bean professorDao for type ProfessorDao

--- src/main/java/com/equipeor/isepu/controller/ProfesseurController.java
+++ src/main/java/com/equipeor/isepu/controller/ProfesseurController.java
@@ -1,13 +1,39 @@
 package com.equipeor.isepu.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+
+
+import com.equipeor.isepu.dao.ProfessorDao;
+import com.equipeor.isepu.model.Professor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
 
 @RestController
 public class ProfesseurController {
-    @RequestMapping(value="/Prof", method= RequestMethod.GET)
-    public String listeEleves() {
-        return "Un exemple de prof";
+
+    @Autowired
+    private ProfessorDao professorDao;
+    @GetMapping(value = "/Professors")
+    public List<Professor> ListeProfessor(){return professorDao.findAll();}
+    @PostMapping(value = "/Professors")
+    public ResponseEntity<Void> AddProfessor(@RequestBody Professor teacher){
+        Professor teacherAdded  =   professorDao.save(teacher);
+        if(teacherAdded==null){
+            return ResponseEntity.noContent().build();
+        }
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(teacherAdded.getId())
+                .toUri();
+
+        return ResponseEntity.ok().build();
     }
 }
