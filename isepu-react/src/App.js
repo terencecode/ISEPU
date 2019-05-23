148377cb032802e56caf1bfaa0e75d634d7f96e7
cleman95 <clement.phu@hotmail.fr>
2019-05-23 13:41:51 +0200

    First components
    add first components

--- isepu-react/src/App.js
+++ isepu-react/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from './components/Navbar';
+import Board from './components/Board';
 function App() {
   return (
     <div className="App">
-      <h1> welcome to the project</h1>
+      <Navbar />
+      <Board />
     </div>
   );
 }
