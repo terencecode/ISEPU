18f72fcd569ea47814157a61759b3d10e7513a32
William Trojanowski <williamtrojanowski.wt@gmail.com>
2019-06-07 01:17:53 +0200

    Refactoring of professor and student controllers

--- src/main/java/com/equipeor/isepu/controller/StudentController.java
+++ src/main/java/com/equipeor/isepu/controller/StudentController.java
@@ -1,41 +1,44 @@
 package com.equipeor.isepu.controller;
 
 import com.equipeor.isepu.model.Student;
-import com.equipeor.isepu.repository.StudentRepository;
+import com.equipeor.isepu.payload.response.UserResponse;
+import com.equipeor.isepu.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.util.Collection;
 
 @RestController
 @RequestMapping("/student")
 public class StudentController {
 
     @Autowired
-    private StudentRepository studentRepository;
+    private StudentService studentService;
 
     @Secured("ROLE_ADMIN")
     @GetMapping("/all")
-    public List<Student> getStudents() {
-        return studentRepository.findAll();
+    public ResponseEntity<Collection<UserResponse>> getStudents() {
+        return ResponseEntity.ok(studentService.getStudents());
     }
 
     @Secured("ROLE_ADMIN")
-    @DeleteMapping("/{id}")
-    public void deleteStudent(@PathVariable int id) {
-        studentRepository.deleteById(id);
+    @DeleteMapping("/{email}")
+    public ResponseEntity<Void> deleteStudent(@PathVariable String email) {
+        studentService.deleteStudent(email);
+        return ResponseEntity.ok().build();
     }
 
     @Secured("ROLE_PROFESSOR")
     @GetMapping(value = "/all/{promo}")
-    public List<Student> getPromo(@PathVariable String promo) {
-        return studentRepository.findByPromo(promo);
+    public ResponseEntity<Collection<UserResponse>> getPromo(@PathVariable String promo) {
+        return ResponseEntity.ok(studentService.getPromo(promo));
     }
 
     @Secured("ROLE_ADMIN")
     @PutMapping("/{id}")
     public void updateStudent(@RequestBody Student student, @PathVariable int id) {
-        studentRepository.save(student);
+        studentService.updateStudent(student,id);
     }
 }
